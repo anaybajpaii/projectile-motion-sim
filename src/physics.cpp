dddfefbe27f1c5a771eff2fccf08c2141f2a458b
Anay Bajpai <anay.bajpai25@gmail.com>
2026-08-29 19:42:59 +0530

    Added variable declarations to, implemented formulas

---
+++ src/physics.cpp
@@ -0,0 +1,41 @@
+#include "Physics.h"
+#include <cmath>
+
+namespace Physics 
+{
+double timeOfFlight(double vy0, double y0, double gravity)
+{
+    double result = (vy0 + std::sqrt(vy0 * vy0 + 2.0 * gravity * y0)) / gravity;
+    return result;
+}
+double maxHeight(double vy0, double gravity) 
+{
+    double result = (vy0 * vy0) / (2.0 * gravity);
+    return result;
+}
+double range(double speed, double angleDegrees, double gravity) 
+{
+    double result = (speed * speed) * std::sin(2 * degreesToRadians(angleDegrees)) / gravity;
+    return result;
+}
+double positionX(double x0, double vx0, double t) 
+{
+    double result = x0 + (vx0 * t);
+    return result;
+}
+double positionY(double y0, double vy0, double t, double gravity)
+{
+    double result = y0 + (vy0 * t) - (0.5 * gravity * t * t);
+    return result;
+}
+double velocityY(double vy0, double t, double gravity)
+{
+    double result = vy0 - (gravity * t);
+    return result;
+}
+double degreesToRadians(double degrees) 
+{
+    double result = degrees * M_PI / 180.0;
+    return result;
+}
+}
---
+++ src/physics.h
@@ -0,0 +1,13 @@
+#ifndef PHYSICS_H
+#define PHYSICS_H
+
+namespace Physics {constexpr double GRAVITY = 9.80665;
+double timeOfFlight(double vy0, double y0, double gravity); 
+double maxHeight(double vy0, double gravity); 
+double range(double speed, double angleDegrees, double gravity); 
+double positionX(double x0, double vx0, double t); 
+double positionY(double y0, double vy0, double t, double gravity); 
+double velocityY(double vy0, double t, double gravity); 
+double degreesToRadians(double degrees);
+}
+#endif
